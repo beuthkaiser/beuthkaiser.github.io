0eb1e002ecf228d311e1e1ec55cc649c7dbbf7b5
beuthkaiser <46374536+beuthkaiser@users.noreply.github.com>
2019-01-14 00:43:21 +0100

    Update createScene.js

--- cog1/createScene.js
+++ cog1/createScene.js
@@ -21,22 +21,23 @@ define(["exports", "scenegraph", "animation","importerOBJ"], //
 
           
             // BEGIN exercise myModel
+          /*
             var myModelNode = scenegraph.createNodeWithModel("king", "king", {
                 scale: 200
             });
             myModelNode.rotateTo([-1.6, 0, 0]);
             myModelNode.translate([0, -400, 0]);
-     /*
+  
                  var cubeNode = scenegraph.createNodeWithModel("cube", "cube", {
                 scale: 100
             }); //300
             cubeNode.rotateTo([1.6, -3.87, 0]);
-     
+     */
      
             var sphereNode = scenegraph.createNodeWithModel("sphere", "sphere", {
                 scale: 200
             });
-            
+            /*   
             // END exercise myModel
 
             //return;
