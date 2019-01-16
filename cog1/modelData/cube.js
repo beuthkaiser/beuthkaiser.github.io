be01a8f3ef593232fc927706d960ee23d087eace
beuthkaiser <46374536+beuthkaiser@users.noreply.github.com>
2019-01-16 15:10:18 +0100

    Update cube.js

--- cog1/modelData/cube.js
+++ cog1/modelData/cube.js
@@ -94,28 +94,29 @@ define(["exports", "data"], function(exports, data) {
 			// Order 0 to 16 form bottom-left to top-right
 			// line by line, indices in spatial order:
 			
-			
-			
+			var o=1/900;//vs seams
+			//some vertices can be shared, some not (seams)
 			instance.textureCoord =
 			[
-			
+			   [1/3+o,1],//0
+ 			   [2/3-o,1],//1
+			   [0,2/3-o],//2
+			   [1/3+o,2/3-o],//3
+			   [2/3-o,2/3-o],//4
+			   [1,2/3-o],//5
+			   [0,1/3+o],//6
+			   [1/3+o,1/3+o],//7
+			   [2/3-o,1/3+o],//8
+			   [1,1/3],//9
+			   [1/3+o,0],//10
+			   [2/3-o,0],//11
+			   [2/3+o,1/3],//12 
+			   [2/3+o,0],//13 
+			   [1,0]//14
+
 				
 			 ];
-			var u,v;
-			for (var y=0;y<=3;y+=1) for (var x=0;x<=3;x+=1)
-			{
-			//fix seams
-			var seamFix = 1/9000;
-			if (x==0){u=0;}
-			if (x==1){u=1/3+seamFix;}
-			if (x==2){u=2/3-seamFix;}
-			if (x==3){u=1;}
-			if (y==0){v=0;}
-			if (y==1){v=1/3+seamFix;}
-			if (y==2){v=2/3-seamFix;}
-			if (y==3){v=1;}
-			instance.textureCoord.push([u,v]);
-			}
+			
 			
 			//console.log(instance.textureCoord);
 			
@@ -127,20 +128,12 @@ define(["exports", "data"], function(exports, data) {
 			// D=bottom/down, U=top/up, F=front, R=right, B=back, L=left
 			// The mapping is explained on the texture image.
 			  instance.polygonTextureCoord = [
-				/*
-				[2,3,7,6], // bottom
-				[9,10,14,1], // top
-				[5,6,10,9], // front 
-				[6,7,11,10], //right
-				[2,3,7,6], // back
-				[4,5,9,8] // left
-				*/
-				[1,2,6,5], // bottom
-				[9,10,14,13], // top
-				[9,5,6,10], // front
-				[6,7,11,10], // right
-				[6,2,3,7], // back
-				[4,5,9,8], // left
+				[10,11,8,7], // bottom
+				[3,4,1,0], // top
+				[3,7,8,4], // front
+				[8,9,5,4], // right
+				[12,13,14,9], // back
+				[6,7,3,2], // left
 											  
 											  ];
 
